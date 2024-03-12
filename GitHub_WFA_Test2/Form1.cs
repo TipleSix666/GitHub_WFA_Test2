b41384c0305dd40878789b6aeaf5edc797d6e041
TripleSix <57644436+TipleSix666@users.noreply.github.com>
2024-03-12 17:08:06 +0300

    Provided method for user age calculation.

--- GitHub_WFA_Test2/Form1.cs
+++ GitHub_WFA_Test2/Form1.cs
@@ -16,5 +16,12 @@ namespace GitHub_WFA_Test2
         {
             InitializeComponent();
         }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int currentYear = int.Parse(textBox1.Text);
+            int birthYear = int.Parse(textBox2.Text);
+            MessageBox.Show($"Ваш возраст: {currentYear - birthYear}");
+        }
     }
 }
